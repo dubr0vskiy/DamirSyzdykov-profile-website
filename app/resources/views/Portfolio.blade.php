6492de440771bed1c804c679bb16a1715e92c158
Damir Syzdykov <190103033@stu.sdu.edu.kz>
2021-02-23 02:04:33 +0600

    laravel

---
+++ app/resources/views/Portfolio.blade.php
@@ -0,0 +1,459 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>portfolio</title>
+
+        <link rel="icon" type="image/x-icon" href="#" />
+       
+        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+       
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        
+        <link href="css/main.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <nav class="navbar navbar-expand-lg text-uppercase fixed-top" id="nav">
+          <a class="navbar-brand" href="#">My Portfolio</a>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ml-auto">
+              <a class="nav-item nav-link py-3 px-0 px-lg-3 rounded" href="#" id="home">Home </a>
+              <a class="nav-item nav-link py-3 px-0 px-lg-3 rounded" href="#" id="about-me">About-me</a>
+              <a class="nav-item nav-link py-3 px-0 px-lg-3 rounded" href="#" id="portfolio">Portfolio</a>
+              <a class="nav-item nav-link py-3 px-0 px-lg-3 rounded" href="#" id="contact-me">Contact-me</a>
+            </div>
+          </div>
+        </nav>
+        <header class="header bg-primary text-white text-center">
+            <div class="container d-flex align-items-center flex-column">
+                
+                <img class="header-avatar mb-5" src="{{asset('avataaaars.svg')}}" alt="" />
+                
+                <h1 class="header-heading text-uppercase mb-0">DAMIR SYZDYKOV</h1>
+                
+                <div class="divider light">
+                    <div class="line"></div>
+                    <div class="icon"><i class="fas fa-star"></i></div>
+                    <div class="line"></div>
+                </div>
+                
+                <p class="header-subheading font-weight-light mb-0">SDU - IS - future programmer</p>
+            </div>
+        </header>
+
+        <section class="about-me text-black mb-0" id="about">
+            <div class="container">
+
+                <h2 class="page-section-heading text-center text-uppercase text-black">About me</h2>
+
+                <div class="divider light">
+                    <div class="line" ></div>
+                    <div class="icon" ><i class="fas fa-star"></i></div>
+                    <div class="line" ></div>
+                </div>
+
+                <div class=" ml-auto"><p class="lead">Hello,My name is Damir,i am from Balkash, it is small town in Karagandy region.I study at Suleyman Demirel University, 2 course of Information Systems.I ended school in Balkash Liceum #15 named by Alikhan Bukeikhanov.From 11'th class i set a goal to study computer science and be a good programmer in the future that help to KZ.</p></div>
+            </div>
+        </section>
+
+        <section class="portfolio">
+            <div class="container">
+                <h2 class="heading text-center text-uppercase text-secondary mb-0">Portfolio</h2>
+                <div class="divider light">
+                    <div class="line"></div>
+                    <div class="icon"><i class="fas fa-star"></i></div>
+                    <div class="line"></div>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-md-6 col-lg-4 mb-5">
+                        <div class="portfolio-item mx-auto">
+                            <div class="row"> 
+                                <div class="portfolio-item-caption d-flex align-items-center justify-content-center p-5"><img class="img-fluid" src="img/hw.jpg" alt="" /></div>
+                            </div>
+                            <div class="row p-5"> 
+                                <a href="lab1/index.html"><button type="button" class="btn btn-outline-primary d-flex align-items-center justify-content-center h-100 w-100">See more</button></a>
+                            </div>                            
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4 mb-5">
+                        <div class="portfolio-item mx-auto">
+                            <div class="row"> 
+                                <div class="portfolio-item-caption d-flex align-items-center justify-content-center p-5" p><img class="img-fluid" src="img/hw.jpg" alt="" /></div>
+                            </div>
+                            <div class="row p-5"> 
+                                <a href="lab2/index.html"><button type="button" class="btn btn-outline-primary d-flex align-items-center justify-content-center h-100 w-100">See more</button></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-lg-4 mb-5">
+                        <div class="portfolio-item mx-auto">
+                            <div class="row "> 
+                                <div class="portfolio-item-caption d-flex align-items-center justify-content-center p-5 "><img class="img-fluid" src="img/hw.jpg" alt=""/></div>
+                            </div>
+                            <div class="row p-5"> 
+                                <a href="lab3/index.html"><button type="button" class="btn btn-outline-primary d-flex align-items-center justify-content-center h-100 w-100">See more</button></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-lg-4 mb-5 mb-lg-0">
+                        <div class="portfolio-item mx-auto">
+                            <div class="row"> 
+                                <div class="portfolio-item-caption d-flex align-items-center justify-content-center p-5"> <img class="img-fluid" src="img/hw.jpg" alt=""/></div>
+                            </div>
+                           <div class="row p-5"> 
+                                <a href="lab4/first assignment/first.html"><button type="button" class="btn btn-outline-primary d-flex align-items-center justify-content-center h-100 w-100">See more</button></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-lg-4 mb-5 mb-md-0">
+                        <div class="portfolio-item mx-auto">
+                            <div class="row"> 
+                                <div class="portfolio-item-caption d-flex align-items-center justify-content-center p-5"><img class="img-fluid" src="img/hw.jpg" alt="" /></div>
+                            </div>
+                            <div class="row p-5"> 
+                                <a href="lab4/second assignment/second.html"><button type="button" class="btn btn-outline-primary d-flex align-items-center justify-content-center h-100 w-100">See more</button></a>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4 ">
+                        <div class="portfolio-item mx-auto">
+                            <div class="row p-5">
+                                <div class="portfolio-item-caption d-flex align-items-center justify-content-center"><img class="img-fluid" src="img/hw.jpg" alt="" />
+                                   
+                                </div>
+                            </div>
+                            <div class="row p-5" > 
+                               <a href="lab4/third assignment/third.html"> <button type="button" class="btn btn-outline-primary d-flex align-items-center justify-content-center h-100 w-100">See more</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="contact-me" id="contact">
+            <div class="container">
+                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Me</h2>
+                               <div class="divider light">
+                    <div class="line"></div>
+                    <div class="icon"><i class="fas fa-star"></i></div>
+                    <div class="line"></div>
+                </div>
+              
+                <div class="row">
+                    <div class="col-lg-8 mx-auto">
+                       
+                        <form id="contactForm" name="sentMessage" novalidate="novalidate">
+                            <div class="control-group">
+                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                                    <label>Name</label>
+                                    <input class="form-control" id="name" type="text" placeholder="Name" required="required" data-validation-required-message="Please enter your name." />
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                                    <label>Email Address</label>
+                                    <input class="form-control" id="email" type="email" placeholder="Email Address" required="required" data-validation-required-message="Please enter your email address." />
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                                    <label>Phone Number</label>
+                                    <input class="form-control" id="phone" type="tel" placeholder="Phone Number" required="required" data-validation-required-message="Please enter your phone number." />
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                                    <label>Message</label>
+                                    <textarea class="form-control" id="message" rows="5" placeholder="Message" required="required" data-validation-required-message="Please enter a message."></textarea>
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <br />
+                            <div id="success"></div>
+                            <div class="form-group"><button class="btn btn-primary btn-xl" id="sendMessageButton" type="submit">Send</button></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+
+<style>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+  line-height: 1.15;
+  -webkit-text-size-adjust: 100%;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+}
+
+article, aside, figcaption, figure, footer, header, hgroup, main, nav, section {
+  display: block;
+}
+
+body {
+  margin: 0;
+  font-family: "Lato", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #212529;
+  text-align: left;
+  background-color: #fff;
+}
+h1, h2, h3, h4, h5, h6 {
+  margin-top: 0;
+  margin-bottom: 0.5rem;
+}
+
+p {
+  margin-top: 0;
+  margin-bottom: 1rem;
+}
+img {
+  vertical-align: middle;
+  border-style: none;
+}
+.header {
+  padding-top: calc(6rem + 74px);
+  padding-bottom: 6rem;
+}
+.header .header-heading {
+  font-size: 2.75rem;
+  line-height: 2.75rem;
+}
+.header .header-subheading {
+  font-size: 1.25rem;
+}
+.header .header-avatar {
+  width: 15rem;
+}
+
+
+.divider {
+  margin: 1.25rem 0 1.5rem;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.divider .line {
+  width: 100%;
+  max-width: 7rem;
+  height: 0.25rem;
+  background-color: #2c3e50;
+  border-radius: 1rem;
+  border-color: #2c3e50 !important;
+}
+.divider .line:first-child {
+  margin-right: 1rem;
+}
+.divider .line:last-child {
+  margin-left: 1rem;
+}
+.divider .icon {
+  color: #2c3e50 !important;
+  font-size: 2rem;
+}
+
+#nav {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  background-color: #2B3856;
+  font-family: "Montserrat", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  font-weight: 700;
+}
+#nav .navbar-brand {
+  color: #fff;
+}
+#nav .nav-item{
+	color: #fff;
+}
+#nav .nav-item:hover{
+	color: #1abc9c;
+}
+.navbar-brand {
+  display: inline-block;
+  padding-top: 0.3125rem;
+  padding-bottom: 0.3125rem;
+  margin-right: 1rem;
+  font-size: 1.25rem;
+  line-height: inherit;
+  white-space: nowrap;
+}
+
+
+
+#about{
+	margin-top: 100px;
+}
+.img-fluid {
+  max-width: 100%;
+  height: auto;
+}
+
+.portfolio .portfolio-item {
+  display: block;
+  max-width: 25rem;
+  border-radius: 0.5rem;
+  background-color: #FBFCFC;
+  border: solid black 2px;
+}
+
+
+#contact{
+	margin-top: 100px;
+}
+.btn{
+	width: 100%;
+	font-size: 2.75rem;
+}
+
+.floating-label-form-group {
+  position: relative;
+  border-bottom: 1px solid #e9ecef;
+}
+.floating-label-form-group input,
+.floating-label-form-group textarea {
+  font-size: 1.5em;
+  position: relative;
+  z-index: 1;
+  padding-right: 0;
+  padding-left: 0;
+  resize: none;
+  border: none;
+  border-radius: 0;
+  background: none;
+  box-shadow: none !important;
+}
+.floating-label-form-group label {
+  font-size: 0.85em;
+  line-height: 1.764705882em;
+  position: relative;
+  z-index: 0;
+  top: 2em;
+  display: block;
+  margin: 0;
+  transition: top 0.3s ease, opacity 0.3s ease;
+  opacity: 0;
+}
+.floating-label-form-group:not(:first-child) {
+  padding-left: 14px;
+  border-left: 1px solid #e9ecef;
+}
+
+.floating-label-form-group-with-value label {
+  top: 0;
+  opacity: 1;
+}
+
+.floating-label-form-group-with-focus label {
+  color: #1abc9c;
+}
+
+form .row:first-child .floating-label-form-group {
+  border-top: 1px solid #e9ecef;
+}
+
+
+@media (min-width: 992px) {
+  #nav {
+    padding-top: 1.5rem;
+    padding-bottom: 1.5rem;
+    transition: padding-top 0.3s, padding-bottom 0.3s;
+  }
+  #nav .navbar-brand {
+    font-size: 1.75em;
+    transition: font-size 0.3s;
+  }
+  #nav .navbar-nav {
+    margin-top: 0;
+  }
+  #nav.navbar-shrink {
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+  }
+  #nav.navbar-shrink .navbar-brand {
+    font-size: 1.5em;
+  }
+  #nav.nav-item{
+  	font-size: 2.75rem;
+
+  }
+}
+.header{
+  padding-top: calc(6rem + 74px);
+  padding-bottom: 6rem;
+}
+.header .header-heading {
+  font-size: 2.75rem;
+  line-height: 2.75rem;
+}
+.header .header-subheading {
+  font-size: 1.25rem;
+}
+.header .header-avatar {
+  width: 15rem;
+}
+
+@media (min-width: 992px) {
+  .header {
+    padding-top: calc(6rem + 104px);
+    padding-bottom: 6rem;
+  }
+  .header .header-heading {
+    font-size: 4rem;
+    line-height: 3.5rem;
+  }
+  .header .header-subheading {
+    font-size: 1.5rem;
+  }
+}</style>
+
+<script>
+
+$("#home").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".header").offset().top},
+        'slow');
+});
+$("#about-me").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#about").offset().top},
+        'slow');
+});
+$("#portfolio").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".portfolio").offset().top},
+        'slow');
+});
+$("#contact-me").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#contact").offset().top},
+        'slow');
+});
+</script>
+
